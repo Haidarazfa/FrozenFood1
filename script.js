028d89eb8d18784d5f10e1af5715f3c67e1c9226
Haidarazfa <113000103+Haidarazfa@users.noreply.github.com>
2025-02-08 00:48:19 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+const products = {
+    "Ayoma": [
+        { name: "Ayoma Rolade Ayam", price: 21000 },
+        { name: "Ayoma Rolade api", price: 22000 }
+    ],
+};
 
+let cart = [];
+
+function showModal(brand) {
+}
